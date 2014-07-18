22026b1b467cef5561af53ef300132e898c3a03c
namchid <ndo@trinity.edu>
2014-07-18 17:41:25 -0400

    fleshed out observe for each of 8 possible reaction types

--- Reactive/Reactor.scala
+++ Reactive/Reactor.scala
@@ -26,10 +26,43 @@ object Reactive {
 ** @params reaction: a final val from Reactive, such as Reactive.MouseClickGetClockTick
 */
 case class Reactor[T](reaction: Int, fn: T => Graphic, framesPerSecond: Int = 0, duration: Double = 0) {
-	val observe = reaction match {
-		case Reactive.ClockTick = Timer.subscribe
-		case Reactive.MouseClick = MouseClick.subscribe
-		case Reactive.MousePosition = MouseClick.subscribe
-		case Reactive.MouseClickX = 
+	val observe: Rx[Any] = reaction match {
+		case Reactive.ClockTick => Timer(framesPerSecond, duration).subscribe
+		case Reactive.MouseClick => MouseClick.subscribe
+		case Reactive.MousePosition => MouseClick.subscribe
+		case Reactive.MouseClickX => {
+			val sub = MouseClick.subscribe
+			Rx {sub()._1}
+		}
+		case Reactive.MouseClickY => {
+			val sub = MouseClick.subscribe
+			Rx {sub()._2}
+		}
+		case Reactive.MousePositionX => {
+			val sub = MousePosition.subscribe
+			Rx {sub()._1}
+		}
+		case Reactive.MousePositionY => {
+			val sub = MousePosition.subscribe
+			Rx {sub()._2}
+		}
+		case Reactive.ClockTickGetMousePosition => {
+			val clock_sub = Timer(framesPerSecond, duration).subscribe
+			val rx = Var(MouseMove.xy())
+			Obs(clock_sub) {
+				rx() = MouseMove.xy()
+			}
+			rx
+		}
+		case Reactive.MouseClickGetClockTick => {
+			val click_sub = MouseClick.subscribe
+			val startTime = new js.Date().getTime()
+			val rx = Var(0.0)
+			Obs(click_sub) {
+				rx() = (new js.Date().getTime() - startTime)/1000 //returns time in seconds
+			}
+			rx
+		}
 	}
+	//todo: based on whatever reactive variable observe is, call fn and pass the appropriate arguments
 }
