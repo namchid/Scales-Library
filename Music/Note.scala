e3bd0631af4dae708dec67a41b1a17b9cee6238a
namchid <ndo@trinity.edu>
2014-07-19 13:47:37 -0400

    skeletons of all methods

--- Music/Note.scala
+++ Music/Note.scala
@@ -3,10 +3,74 @@ import rx._
 
 object Audio {
 	def getAudioContext(): js.Dynamic = js.Dynamic.newInstance(js.Dynamic.global.AudioContext)
+
+	type AudioRampOption: String
+	final val Ramp: AudioRampOption = "ramp"
+	final val LinearRamp: AudioRampOption = "linear"
+	final val NoRamp: AudioRampOption = "none"
 }
 
 case class Note(val freq: Double = 0, val vol: Double = 1) {
+	val ctx = Audio.getAudioContext()
+	val currentNode: Var[js.Dynamic] = Var(null)
 
+	def currentTime(): Double = {
+		val time = ctx.currentTime.toString()
+		return time.toDouble
+	}
+
+	def play(time: Double = 0): Unit = {
+		//todo
+	}
+
+	def stop(delay: Double = 0): Unit = {
+		//todo
+	}
+
+	import Audio._
+	def playRamp(start: Double, duration: Double, targetFrequency: Double = 0, rampOption: AudioRampOption = Audio.NoRamp): Unit = {
+		//todo
+	}
+
+	def playBeats(start: Double = 0, times: Int = 1, beatDuration: Double = .2, beatPause: Double = .2): Unit = {
+		//todo
+	}
+
+	private def playBeatsHelper(beatDuration: Double, beatPause: Double, timesPlayed: Int, totalTimes: Int, currTime: Double): Unit = {
+		//todo
+	}
+
+	def >(octaves: Int = 1): Note = {
+		//todo
+	}
+
+	def <(octaves: Int = 1): Note = {
+
+	}
+
+	def >>>>: Note = {
+		//todo
+	}
+
+	def <<<<: Note = {
+		//todo
+	}
+
+	def setVolume(vol: Double): Note = {
+		//todo
+	}
+
+	def louder(other: Note): Boolean = {
+		//todo
+	}
+
+	def higherFrequency(other: Note): Boolean = {
+		//todo
+	}
+
+	def frequency: Double = freq
+
+	def volume: Double = vol
 }
 
 //'s' after note indicates sharp
