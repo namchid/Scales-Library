80e3579c1691488d5d2f0b510798daf9bbb20332
namchid <ndo@trinity.edu>
2014-07-22 21:51:18 -0400

    updated demo

--- Demo/Scala-JS-App/Demo.scala
+++ Demo/Scala-JS-App/Demo.scala
@@ -6,135 +6,8 @@ import org.scalajs.dom
 import rx._
 
 import edu.depauw.scales.graphics._
+import edu.depauw.scales.reactive._
 import Base._
-
-object Canvas {
-  val canvas = dom.document.createElement("canvas").asInstanceOf[dom.HTMLCanvasElement]
-  canvas.width = 500
-  canvas.height = 500
-  val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
-
-  val playground = dom.document.getElementById("playground")
-  playground.appendChild(canvas)
-}
-
-object GraphicHolder {
-  val ctx = Canvas.ctx
-  val width = Canvas.canvas.width
-  val height = Canvas.canvas.height
-
-  val graphics: Var[List[Graphic]] = Var(Nil)
-  val fillColors: Var[List[String]] = Var(Nil)
-  val strokeStyles: Var[List[String]] = Var(Nil)
-  val lineWidths: Var[List[Int]] = Var(Nil)
-
-  def getIndex(): Int = {
-    return graphics().size
-  }
-
-  def addGraphic(g: Graphic): Unit = { 
-    graphics() = graphics() :+ g 
-  }
-
-  def setFillColor(index: Int, color: String): Unit = {
-    fillColors() = fillColors().patch(index, List(color), 1)
-  }
-
-  def setStrokeColor(index: Int, color: String): Unit = {
-    strokeStyles() = strokeStyles().patch(index, List(color), 1)
-  }
-
-  def setWidth(index: Int, width: Int): Unit = {
-    lineWidths() = lineWidths().patch(index, List(width), 1)
-  }
-
-  def setAll(index: Int, fillColor: String, strokeColor: String, width: Int): Unit = {
-    fillColors() = fillColors().patch(index, List(fillColor), 1)
-    strokeStyles() = strokeStyles().patch(index, List(strokeColor), 1)
-    lineWidths() = lineWidths().patch(index, List(width), 1)
-  }
-
-  def updateGraphic(index: Int, g: Graphic): Unit = {
-    graphics() = graphics().patch(index, List(g), 1)
-  }
-
-  Obs(graphics) {
-    ctx.clearRect(0, 0, width, height)
-    for(i <- 0 until graphics().length) {
-      ctx.fillStyle = fillColors()(i)
-      ctx.strokeStyle = strokeStyles()(i)
-      ctx.lineWidth = lineWidths()(i)
-      graphics()(i).render(ctx)
-    }
-  }
-}
-
-/////////////////////////////////////////////////// subscriptions
-object MouseClick {
-  val xy = Var((0, 0))
-  val x = Rx { xy()._1 }
-  val y = Rx { xy()._2 }
-
-  private def listen() = Rx {
-    dom.onclick = { 
-      (e: dom.MouseEvent) => xy() = (e.clientX.toInt, e.clientY.toInt)
-    }
-  }
-
-  listen()
-
-  def subscribe(): Rx[(Int, Int)] = xy
-  def subscribeX(): Rx[Int] = x
-  def subscribeY(): Rx[Int] = y
-}
-
-/////////////////////////////////////////////////// reactives
-object Reactive {
-  type ReactionType = Int
-
-  final val ClockTick: ReactionType = 0
-  final val MouseClick: ReactionType = 1
-  final val MousePosition: ReactionType = 2
-
-  final val MouseClickX: ReactionType = 3
-  final val MouseClickY: ReactionType = 4
-  final val MousePositionX: ReactionType = 5
-  final val MousePositionY: ReactionType = 6
-
-  final val ClockTickGetMousePosition: ReactionType = 7
-  final val MouseClickGetClockTime: ReactionType = 8
-  final val MousePositionGetClockTime: ReactionType = 9
-}
-
-case class Reactor[T](reaction: Reactive.ReactionType, fn: T => Graphic, fillStyle: String = "grey",
-  strokeStyle: String = "black", width: Int = 1, framesPerSecond: Double = 1, duration: Double = 0) {
-
-  val function = fn.asInstanceOf[(Any => Graphic)]
-
-  val target: Rx[Any] = reaction match {
-    case Reactive.MouseClick => MouseClick.subscribe
-    case Reactive.MouseClickX => MouseClick.subscribeX
-    case Reactive.MouseClickY => MouseClick.subscribeY
-    case _ => Var(0)
-  } 
-
-  val index = Var(-1)
-
-  private def initGraphic(g: Graphic): Unit = {
-    GraphicHolder.setAll(index(), fillStyle, strokeStyle, width)
-    GraphicHolder.addGraphic(g)
-  }
-
-  Obs(target) {
-    val g = function(target())
-    if(index() == -1) {
-      index() = GraphicHolder.getIndex
-      initGraphic(g)
-    } else {
-      GraphicHolder.updateGraphic(index(), g)
-    }
-  }
-}
 
 
 object ScalaJSExample extends js.JSApp {
@@ -147,19 +20,50 @@ object ScalaJSExample extends js.JSApp {
 
 /////////////////// tehehehehe
     def fnMouse(xy: (Int, Int)): Graphic = {
-      Rectangle(100, 200).top.translate(xy._1, xy._2)
+      Rectangle(100, 200).translate(xy._1 - 25, xy._2 - 100)
     }
 
     def fnMouseX(x: Int): Graphic = {
-      Rectangle(50, 100).translate(x, 300)
+      Rectangle(50, 100).r.translate(x + 25, 300)
     }
 
     def fnMouseY(y: Int): Graphic = {
-      Rectangle(50, 100).translate(250, y)
+      Rectangle(50, 100).b.translate(250, y - 100)
     }
 
-    Reactor(Reactive.MouseClick, fnMouse)
-    Reactor(Reactive.MouseClickX, fnMouseX, "blue")
-    Reactor(Reactive.MouseClickY, fnMouseY, "red")
+    def fnTime(t: Double): Graphic = {
+      (Ellipse(20, 30) atop Rectangle(40, 40)).translate(t * 30, 300)
+    }
+
+    def fnKey(x: Int): Graphic = {
+      Ellipse(45, 60).translate(200, x * 30)
+    }
+
+    def fnKeyAny(x: Int): Graphic = {
+      Ellipse(25, 25).translate(x * 10, x * 10)
+    }
+
+    def fnPos(xy: (Int, Int)): Graphic = {
+      Ellipse(25, 25).translate(xy._1 - 5, xy._2 - 145)
+    }
+
+    import Reactive._
+    // Reactor(MouseClick, fnMouse, "green")
+    // Reactor(ClockTick(2, 20), fnTime, "red")
+    // Reactor(KeyPress(Key.Space), fnKey, "orange", "grey", 5)
+    // Reactor(KeyPress, fnKeyAny, "blue", "magenta")
+    // Reactor(MousePosition, fnPos, "rgb(65, 123, 142)", "black", 3)
+
+    // Reactor(MouseClickX, fnMouseX, "blue")
+    // Reactor(MouseClickY, fnMouseY, "rgb(70, 200, 70)")
+    // Reactor(MousePositionX, fnMouseX, "rgb(133, 60, 200)")
+    // Reactor(MousePositionY, fnMouseY, "rgb(155, 140, 10)")
+
+    Reactor(ClockTickGetMousePosition(1, 100), fnPos, "yellow", "green", 4)
+    Reactor(MouseClickGetClockTime, fnTime, "magenta", "grey", 3)
+    Reactor(MousePositionGetClockTime, fnTime, "rgb(200, 200, 55)", "white", 0)
+    Reactor(KeyPressGetClockTime(Key.A), fnTime, "black", "blue")
+    Reactor(KeyPressGetClockTime, fnTime, "green") //todo
+
   }
 }
